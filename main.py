7c95a3079c97237dd95f7c14e06734fde060119c
Julia <julialov2001@gmail.com>
2022-12-15 22:29:03 +0300

    Analyze multiple files

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -13,37 +12,53 @@ def chunk_list_to_string(chunk_list, start_index):
     return "".join(chunk_list_in_order)
 
 
-MAX_CHUNK_LENGTH = 4
-results = dict()
-chunk_length = 0
-start_index = 0
-chunk_list = [''] * MAX_CHUNK_LENGTH
-with open(args.input) as file:
-    for line in file:
-        for c in line:  # \n is space!
-            if c.isspace():
-                chunk_length = 0
-                start_index = 0
-            else:
-                if chunk_length == 4:
-                    chunk_list[start_index] = c
-                    start_index = (start_index + 1) % MAX_CHUNK_LENGTH
-                    chunk_string = chunk_list_to_string(chunk_list, start_index)
-                    if chunk_string in results:
-                        results[chunk_string] += 1
-                    else:
-                        results[chunk_string] = 1
+def analyze_file(filename):
+    MAX_CHUNK_LENGTH = 4
+    results = dict()
+    chunk_length = 0
+    start_index = 0
+    chunk_list = [''] * MAX_CHUNK_LENGTH
+    with open(filename) as file:
+        for line in file:
+            for c in line:  # \n is space!
+                if c.isspace():
+                    chunk_length = 0
+                    start_index = 0
                 else:
-                    chunk_list[(start_index + chunk_length) % MAX_CHUNK_LENGTH] = c
-                    chunk_length += 1
                     if chunk_length == 4:
+                        chunk_list[start_index] = c
+                        start_index = (start_index + 1) % MAX_CHUNK_LENGTH
                         chunk_string = chunk_list_to_string(chunk_list, start_index)
                         if chunk_string in results:
                             results[chunk_string] += 1
                         else:
                             results[chunk_string] = 1
-keys = list(results.keys())
-for key in keys:
-    if results[key] < 2:
-        del results[key]
-print(results)
+                    else:
+                        chunk_list[(start_index + chunk_length) % MAX_CHUNK_LENGTH] = c
+                        chunk_length += 1
+                        if chunk_length == 4:
+                            chunk_string = chunk_list_to_string(chunk_list, start_index)
+                            if chunk_string in results:
+                                results[chunk_string] += 1
+                            else:
+                                results[chunk_string] = 1
+    return results
+
+
+def get_non_unique_set(results):
+    non_unique_set = set()
+    for key, value in results.items():
+        if value >= 2:
+            non_unique_set.add(key)
+    return non_unique_set
+
+input_results = analyze_file(args.input)
+input_set = get_non_unique_set(input_results)
+corpus_results = analyze_file(args.corpus)
+corpus_set = get_non_unique_set(corpus_results)
+print(input_results)
+print(input_set)
+print(corpus_results)
+print(corpus_set)
+print(input_set & corpus_set)
+
