563918c48a56e9f40d05dc84337437652e2da04f
Julia <julialov2001@gmail.com>
2022-12-16 14:22:04 +0300

    Implement TextAnalyzer class

--- main.py
+++ main.py
@@ -1,64 +1,51 @@
 import argparse
+from text_analyzer import TextAnalyzer
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--input")
 parser.add_argument("--corpus")
 args = parser.parse_args()
 
+try:
+    input_file_chunk_set = None
+    with open(args.input) as input_file:
+        input_text_analyzer = TextAnalyzer()
+        for line in input_file:
+            input_text_analyzer.process_line(line)
+        input_file_chunk_set = input_text_analyzer.chunk_set
 
-def chunk_list_to_string(chunk_list, start_index):
-    chunk_list_in_order = chunk_list[start_index:]
-    chunk_list_in_order.extend(chunk_list[:start_index])
-    return "".join(chunk_list_in_order)
+    text_number = 1
+    corpus_text_data = []
+    with open(args.corpus) as corpus_file:
+        corpus_text_analyzer = TextAnalyzer()
+        for line in corpus_file:
+            if line.startswith("-") and line.rstrip() == "-":
+                top_chunks = corpus_text_analyzer.top_chunks
+                text_results = f"[{text_number}] "
+                text_results += ','.join(map(lambda chunk_data: f"{chunk_data[0]} ({chunk_data[1]} entries)", top_chunks))
+                print(text_results)
+                corpus_text_data.append(set(chunk for chunk, frequency in top_chunks))
+                text_number += 1
+                corpus_text_analyzer = TextAnalyzer()
+            else:
+                corpus_text_analyzer.process_line(line)
+        top_chunks = corpus_text_analyzer.top_chunks
+        text_results = f"[{text_number}] "
+        text_results += ','.join(map(lambda chunk_data: f"{chunk_data[0]} ({chunk_data[1]} entries)", top_chunks))
+        print(text_results)
+        corpus_text_data.append(set(chunk for chunk, frequency in top_chunks))
 
+    most_similar = []
+    highest_similarity_level = 1
+    print(input_file_chunk_set)
+    for i in range(0, len(corpus_text_data)):
+        similarity_level = len(input_file_chunk_set.intersection(corpus_text_data[i]))
+        if similarity_level > highest_similarity_level:
+            highest_similarity_level = similarity_level
+            most_similar = [i + 1]
+        elif similarity_level == highest_similarity_level:
+            most_similar.append(i + 1)
 
-def analyze_file(filename):
-    MAX_CHUNK_LENGTH = 4
-    results = dict()
-    chunk_length = 0
-    start_index = 0
-    chunk_list = [''] * MAX_CHUNK_LENGTH
-    with open(filename) as file:
-        for line in file:
-            for c in line:  # \n is space!
-                if c.isspace():
-                    chunk_length = 0
-                    start_index = 0
-                else:
-                    if chunk_length == 4:
-                        chunk_list[start_index] = c
-                        start_index = (start_index + 1) % MAX_CHUNK_LENGTH
-                        chunk_string = chunk_list_to_string(chunk_list, start_index)
-                        if chunk_string in results:
-                            results[chunk_string] += 1
-                        else:
-                            results[chunk_string] = 1
-                    else:
-                        chunk_list[(start_index + chunk_length) % MAX_CHUNK_LENGTH] = c
-                        chunk_length += 1
-                        if chunk_length == 4:
-                            chunk_string = chunk_list_to_string(chunk_list, start_index)
-                            if chunk_string in results:
-                                results[chunk_string] += 1
-                            else:
-                                results[chunk_string] = 1
-    return results
-
-
-def get_non_unique_set(results):
-    non_unique_set = set()
-    for key, value in results.items():
-        if value >= 2:
-            non_unique_set.add(key)
-    return non_unique_set
-
-input_results = analyze_file(args.input)
-input_set = get_non_unique_set(input_results)
-corpus_results = analyze_file(args.corpus)
-corpus_set = get_non_unique_set(corpus_results)
-print(input_results)
-print(input_set)
-print(corpus_results)
-print(corpus_set)
-print(input_set & corpus_set)
-
+    print(f"[{', '.join(map(str, most_similar))}]")
+except IOError as e:
+    print(f"Couldn't open file {e.filename}")
---
+++ text_analyzer.py
@@ -0,0 +1,58 @@
+class TextAnalyzer:
+    _MAX_CHUNK_LENGTH = 4
+    _MOST_POPULAR_NUMBER = 10
+
+    def __init__(self):
+        self._chunk_list = [''] * self._MAX_CHUNK_LENGTH
+        self._results = dict()
+        self._chunk_length = 0
+        self._start_index = 0
+
+    def process_line(self, line):
+        for c in line:
+            self.process_character(c)
+
+    def process_character(self, c):
+        if c.isspace():
+            self._chunk_length = 0
+            self._start_index = 0
+        else:
+            if self._chunk_length == 4:
+                self._chunk_list[self._start_index] = c
+                self._start_index = (self._start_index + 1) % self._MAX_CHUNK_LENGTH
+                self._save_chunk()
+            else:
+                self._chunk_list[(self._start_index + self._chunk_length)
+                                 % self._MAX_CHUNK_LENGTH] = c
+                self._chunk_length += 1
+                if self._chunk_length == 4:
+                    self._save_chunk()
+
+    def _save_chunk(self):
+        chunk_string = self._current_chunk_string()
+        if chunk_string in self._results:
+            self._results[chunk_string] += 1
+        else:
+            self._results[chunk_string] = 1
+
+    def _current_chunk_string(self):
+        chunk_list_in_order = self._chunk_list[self._start_index:]
+        chunk_list_in_order.extend(self._chunk_list[:self._start_index])
+        return "".join(chunk_list_in_order)
+
+    @property
+    def top_chunks(self):
+        sorted_chunks = sorted(filter(lambda item: item[1] > 1, self._results.items()),
+                               key=lambda item: item[1], reverse=True)
+        if len(sorted_chunks) > self._MOST_POPULAR_NUMBER:
+            return sorted_chunks[:self._MOST_POPULAR_NUMBER]
+        return sorted_chunks
+
+    @property
+    def chunk_set(self):
+        return set(self._results.keys())
+        # non_unique_set = set()
+        # for key, value in self._results.items():
+        #     if value >= 2:
+        #         non_unique_set.add(key)
+        # return non_unique_set
